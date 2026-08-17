6544dd3a3f112feebc828e5f5ac00a7b4783c3a2
anaramon9503-max <anaramon9503@gmail.com>
2026-08-17 16:14:51 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// La sección completa de servicios funciona como un único menú desplegable.
-const menu = document.querySelector('.services-menu');
-if (menu) {
-  menu.addEventListener('toggle', () => {
-    // Se deja preparado por si después quieres agregar animaciones o categorías.
-  });
-}
