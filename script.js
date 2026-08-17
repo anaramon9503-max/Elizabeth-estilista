21284755571c0a59a7bad26c421d43c4fba8ee9a
anaramon9503-max <anaramon9503@gmail.com>
2026-08-17 13:29:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('.service').forEach((item)=>{item.addEventListener('toggle',()=>{if(!item.open)return;document.querySelectorAll('.service').forEach((other)=>{if(other!==item)other.open=false;});});});
