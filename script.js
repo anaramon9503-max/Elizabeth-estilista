21f8cd294a757d960fa1b4e103fce1f9b8dc7763
anaramon9503-max <anaramon9503@gmail.com>
2026-08-17 20:50:08 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.service-group').forEach((group) => {
-  group.addEventListener('toggle', () => {
-    if (!group.open) return;
-    document.querySelectorAll('.service-group').forEach((other) => {
-      if (other !== group) other.open = false;
-    });
-  });
-});
